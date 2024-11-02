9d8a2c4759dd896cccdbaa0e752577b76c80f0ff
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-02 16:29:25 +0700

    refactor: items center and switch theme mode

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,10 +1,17 @@
+'use client';
+
+import { useTheme } from 'next-themes';
 import React from 'react';
+import { Switch } from './ui/switch';
 
 function Navigation() {
-  return (
-    <div className="flex p-4 justify-between border-b dark:border-neutral-400">
-      <div>hamzahraihan</div>
+  const { theme, setTheme } = useTheme();
+  console.log('🚀 ~ Navigation ~ theme:', theme);
 
+  return (
+    <div className="flex p-4 justify-between items-center border-b dark:border-neutral-800">
+      <div>hamzahraihan</div>
+      <Switch onClick={() => setTheme(theme == 'dark' ? 'light' : 'dark')} checked={theme == 'dark' ? false : true} aria-readonly />
       <div className="flex gap-20">
         <div>What i do</div>
         <div>Who am i</div>
