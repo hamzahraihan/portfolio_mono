b63d21f199322256daa21184446c706c5385f219
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-02 12:12:45 +0700

    create a navigation bar

---
+++ src/components/navigation.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Navigation() {
+  return (
+    <div className="flex p-4 justify-between border-b dark:border-neutral-400">
+      <div>hamzahraihan</div>
+
+      <div className="flex gap-20">
+        <div>What i do</div>
+        <div>Who am i</div>
+        <div>Talk to me</div>
+      </div>
+    </div>
+  );
+}
+
+export default Navigation;
