778212ab8d8717562106190036b25007849f2c26
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-03 12:59:19 +0700

    Add animation border in navigation bar

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -9,13 +9,25 @@ function Navigation() {
   console.log('🚀 ~ Navigation ~ theme:', theme);
 
   return (
-    <div className="flex p-4 justify-between items-center border-b dark:border-neutral-800">
+    <div className="flex justify-between items-center border-b dark:border-neutral-800 text-sm h-10 mx-4">
       <div>hamzahraihan</div>
-      <Switch onClick={() => setTheme(theme == 'dark' ? 'light' : 'dark')} checked={theme == 'dark' ? false : true} aria-readonly />
-      <div className="flex gap-20">
-        <div>What i do</div>
-        <div>Who am i</div>
-        <div>Talk to me</div>
+      <Switch onClick={() => setTheme(theme == 'dark' ? 'light' : 'dark')} checked={theme != 'dark'} aria-readonly />
+      <div className="flex gap-20 h-full">
+        <div className="after:duration-200 ease-out after:block after:h-0.5 after:w-full after:origin-bottom-right after:scale-x-0 after:bg-neutral-900 after:dark:bg-neutral-300 after:transition-transform after:hover:origin-bottom-left after:hover:scale-x-100">
+          <div className="flex h-full items-center">
+            <p>What i do</p>
+          </div>
+        </div>
+        <div className="after:duration-200 ease-out after:block after:h-0.5 after:w-full after:origin-bottom-right after:scale-x-0 after:bg-neutral-900 after:dark:bg-neutral-300 after:transition-transform after:hover:origin-bottom-left after:hover:scale-x-100">
+          <div className="flex h-full items-center">
+            <p>Who am i</p>
+          </div>
+        </div>
+        <div className="after:duration-200 ease-out after:block after:h-0.5 after:w-full after:origin-bottom-right after:scale-x-0 after:bg-neutral-900 after:dark:bg-neutral-300 after:transition-transform after:hover:origin-bottom-left after:hover:scale-x-100">
+          <div className="flex h-full items-center">
+            <p>Talk to me</p>
+          </div>
+        </div>
       </div>
     </div>
   );
