fbe7a08d33fde9485b44998ed8caaa814726020b
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-03 20:22:44 +0700

    add smooth scroll effect as component

---
+++ src/components/smooth-scroll.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { useSpring, useTransform, useScroll, motion } from 'framer-motion';
+import ResizeObserver from 'resize-observer-polyfill';
+import { ReactNode, useCallback, useLayoutEffect, useRef, useState } from 'react';
+
+type PhysicsProp = {
+  damping: number;
+  mass: number;
+  stiffness: number;
+};
+
+const SmoothScroll = ({ children }: { children: ReactNode }) => {
+  const scrollRef = useRef(null);
+
+  const [pageHeight, setPageHeight] = useState(0);
+
+  const resizePageHeight = useCallback((entries: ResizeObserverEntry[]) => {
+    for (const entry of entries) {
+      setPageHeight(entry.contentRect.height);
+    }
+  }, []);
+
+  useLayoutEffect(() => {
+    const resizeObserber = new ResizeObserver((entries) => resizePageHeight(entries));
+    if (scrollRef?.current) {
+      resizeObserber.observe(scrollRef.current);
+    }
+    return () => resizeObserber.disconnect();
+  }, [scrollRef, resizePageHeight]);
+
+  const { scrollY } = useScroll();
+
+  const transform = useTransform(scrollY, [0, pageHeight], [0, -pageHeight]);
+  const physics: PhysicsProp = { damping: 15, mass: 0.27, stiffness: 55 };
+  const spring = useSpring(transform, physics);
+
+  return (
+    <>
+      <motion.div ref={scrollRef} style={{ y: spring }} className="w-full will-change-transform">
+        {children}
+      </motion.div>
+      <div style={{ height: pageHeight }} />
+    </>
+  );
+};
+
+export default SmoothScroll;
