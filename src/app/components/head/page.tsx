c05cf963495fcbc89e5872b1e856977ec38f3e42
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-03 15:41:48 +0700

    add head screen

---
+++ src/app/components/head/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function HeadPage() {
+  return (
+    <div className="flex gap-20 h-screen justify-center ">
+      <div className="flex items-center justify-center h-fit w-96">
+        <h1 className="text-2xl">a highly motivated developer with a passion for web development. I am currently seeking new opportunities to expand my skills and contribute to the field of technology.</h1>
+      </div>
+      <div className="w-96">a highly motivated developer with a passion for web development. I am currently seeking new opportunities to expand my skills and contribute to the field of technology.</div>
+    </div>
+  );
+}
+
+export default HeadPage;
