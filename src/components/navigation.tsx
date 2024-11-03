b2dd743c6ca29d8a4745afcebb60b23557da5edd
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-03 15:42:54 +0700

    refactor: fix switch turn on dark mode

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -9,9 +9,9 @@ function Navigation() {
   console.log('🚀 ~ Navigation ~ theme:', theme);
 
   return (
-    <div className="flex justify-between items-center border-b dark:border-neutral-800 text-sm h-10 mx-4">
+    <div className="flex sticky top-0 justify-between items-center border-b dark:border-neutral-800 text-sm h-12 mx-4">
       <div>hamzahraihan</div>
-      <Switch onClick={() => setTheme(theme == 'dark' ? 'light' : 'dark')} checked={theme != 'dark'} aria-readonly />
+      <Switch onClick={() => setTheme(theme == 'dark' ? 'light' : 'dark')} checked={theme != 'light'} aria-readonly />
       <div className="flex gap-20 h-full">
         <div className="after:duration-200 ease-out after:block after:h-0.5 after:w-full after:origin-bottom-right after:scale-x-0 after:bg-neutral-900 after:dark:bg-neutral-300 after:transition-transform after:hover:origin-bottom-left after:hover:scale-x-100">
           <div className="flex h-full items-center">
