3abb35b2d93b3151ca886ce675f5f0334db6c100
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-03 16:20:06 +0700

    add more information text to head screen

--- src/app/components/head/page.tsx
+++ src/app/components/head/page.tsx
@@ -1,12 +1,17 @@
+import CircularText from '@/components/circular-text';
 import React from 'react';
 
 function HeadPage() {
   return (
-    <div className="flex gap-20 h-screen justify-center ">
-      <div className="flex items-center justify-center h-fit w-96">
-        <h1 className="text-2xl">a highly motivated developer with a passion for web development. I am currently seeking new opportunities to expand my skills and contribute to the field of technology.</h1>
+    <div className="flex gap-20 h-screen justify-center mt-10">
+      <div className="flex flex-col gap-5 h-fit w-96">
+        <h1 className="text-2xl">Hi, my name is Hamzah Raihan I&apos;m a Front-End Developer</h1>
+        <p>a highly motivated developer with a passion for web development. I am currently seeking new opportunities to expand my skills and contribute to the field of technology.</p>
+        <p>Scroll</p>
       </div>
-      <div className="w-96">a highly motivated developer with a passion for web development. I am currently seeking new opportunities to expand my skills and contribute to the field of technology.</div>
+      <div className="w-36">
+        <CircularText text="hamzahraihan-softwareengineer-" className="motion-safe:animate-spin-slow duration-1000 dark:text-white" />
+      </div>
     </div>
   );
 }
