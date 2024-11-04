3158610e518568f85a81c745f1cce26bd7f47288
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-04 10:35:30 +0700

    add scroll icon with animation

--- src/app/components/head/index.tsx
+++ src/app/components/head/index.tsx
@@ -1,4 +1,6 @@
 import CircularText from '@/components/circular-text';
+import { Scroll } from '@/components/scroll';
+
 import React from 'react';
 
 function HeadPage() {
@@ -7,6 +9,9 @@ function HeadPage() {
       <div className="flex flex-col gap-5 h-fit w-96 mt-10">
         <h1 className="text-2xl">Hi, my name is Hamzah Raihan I&apos;m a Front-End Developer</h1>
         <p>a highly motivated developer with a passion for web development. I am currently seeking new opportunities to expand my skills and contribute to the field of technology.</p>
+        <div className="h-10 w-10">
+          <Scroll />
+        </div>
       </div>
       <div className="w-fit">
         <CircularText text="HAMZAHRAIHAN-SOFTWAREENGINEER-" className="motion-safe:animate-spin-slow duration-1000 dark:text-white" />
