eef4d1d7a9ad13f8553db489a8e38088716e64d0
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-04 16:12:20 +0700

    add motion framer animation to head component

--- src/app/components/head/index.tsx
+++ src/app/components/head/index.tsx
@@ -1,21 +1,23 @@
+'use client';
+
 import CircularText from '@/components/circular-text';
 import { Scroll } from '@/components/scroll';
-
+import { motion } from 'framer-motion';
 import React from 'react';
 
 function HeadPage() {
   return (
-    <div className="flex container mx-auto h-[40rem] gap-20 justify-center mt-10">
-      <div className="flex flex-col gap-5 h-fit w-96 mt-10">
+    <div className="flex container mx-auto h-[32rem] gap-20 justify-center mt-10">
+      <motion.div className="flex flex-col gap-5 h-fit w-96 mt-10" initial={{ opacity: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 1.5 }}>
         <h1 className="text-2xl">Hi, my name is Hamzah Raihan I&apos;m a Front-End Developer</h1>
         <p>a highly motivated developer with a passion for web development. I am currently seeking new opportunities to expand my skills and contribute to the field of technology.</p>
         <div className="h-10 w-10">
           <Scroll />
         </div>
-      </div>
-      <div className="w-fit">
+      </motion.div>
+      <motion.div className="w-fit" initial={{ opacity: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 2 }}>
         <CircularText text="HAMZAHRAIHAN-SOFTWAREENGINEER-" className="motion-safe:animate-spin-slow duration-1000 dark:text-white" />
-      </div>
+      </motion.div>
     </div>
   );
 }
