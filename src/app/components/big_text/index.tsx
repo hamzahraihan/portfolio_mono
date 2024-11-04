dc1f90f9d5a2a0303a9a1c5010ebb057e057d1e5
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-04 16:23:13 +0700

    add new file to import everything in main component

--- src/app/components/big_text/index.tsx
+++ src/app/components/big_text/index.tsx
@@ -1,12 +1,1 @@
-import React from 'react';
-
-function BigText({ title = '' }: Readonly<{ title: string }>) {
-  return (
-    <div className="text-center text-[10rem] max-w-full h-fit text-wrap">
-      <span className="animate-flash ">{'>'}</span>
-      {title}
-    </div>
-  );
-}
-
-export default BigText;
+export * from './big-text';
