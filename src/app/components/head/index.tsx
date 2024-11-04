d0a7b662b4b346173b6a4ab79c049710ab3062d7
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-04 09:27:31 +0700

    refactor: change scroll text to scroll animation using lottie lib

--- src/app/components/head/index.tsx
+++ src/app/components/head/index.tsx
@@ -7,7 +7,6 @@ function HeadPage() {
       <div className="flex flex-col gap-5 h-fit w-96 mt-10">
         <h1 className="text-2xl">Hi, my name is Hamzah Raihan I&apos;m a Front-End Developer</h1>
         <p>a highly motivated developer with a passion for web development. I am currently seeking new opportunities to expand my skills and contribute to the field of technology.</p>
-        <p>Scroll</p>
       </div>
       <div className="w-fit">
         <CircularText text="HAMZAHRAIHAN-SOFTWAREENGINEER-" className="motion-safe:animate-spin-slow duration-1000 dark:text-white" />
