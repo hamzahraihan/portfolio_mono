c50c8039083f9d2f2e4eb949f5e401c26ea0f366
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-04 19:57:01 +0700

    add animation fade in  active after scroll

--- src/app/components/big_text/big-text.tsx
+++ src/app/components/big_text/big-text.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+'use client';
+
+import { motion } from 'framer-motion';
 
 function BigText({ title = '' }: Readonly<{ title: string }>) {
   return (
-    <div className="text-center text-[10rem] max-w-full h-fit text-wrap">
+    <motion.div className="text-center text-[10rem] max-w-full h-fit text-wrap" initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} viewport={{ once: true, amount: 0.8 }}>
       <span className="animate-flash ">{'>'}</span>
       {title}
-    </div>
+    </motion.div>
   );
 }
 
