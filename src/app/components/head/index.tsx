a5e9a87b0e17ddabd7fc7eec418ed3113f6bbdf9
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-04 15:49:37 +0700

    refactor: add container, margin, and height utility to head component

--- src/app/components/head/index.tsx
+++ src/app/components/head/index.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 function HeadPage() {
   return (
-    <div className="flex gap-20 h-96 justify-center mt-10">
+    <div className="flex container mx-auto h-[40rem] gap-20 justify-center mt-10">
       <div className="flex flex-col gap-5 h-fit w-96 mt-10">
         <h1 className="text-2xl">Hi, my name is Hamzah Raihan I&apos;m a Front-End Developer</h1>
         <p>a highly motivated developer with a passion for web development. I am currently seeking new opportunities to expand my skills and contribute to the field of technology.</p>
