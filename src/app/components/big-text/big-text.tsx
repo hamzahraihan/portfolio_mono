857939e8795511e0089941d2db6bbdb44610ce18
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-05 12:02:37 +0700

    change animation from fade in to slide up

--- src/app/components/big-text/big-text.tsx
+++ src/app/components/big-text/big-text.tsx
@@ -1,13 +1,25 @@
 'use client';
 
-import { motion } from 'framer-motion';
+import { motion, useScroll, useTransform } from 'framer-motion';
+import { useRef } from 'react';
 
 function BigText({ title = '' }: Readonly<{ title: string }>) {
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ['start end', 'center center'],
+  });
+
+  // Transform scrollYProgress (0 to 1) into y position (100px to 0)
+  const yPos = useTransform(scrollYProgress, [0, 1], ['100%', '0%']);
+
   return (
-    <motion.div className="text-center text-[10rem] max-w-full h-fit text-wrap" initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} viewport={{ once: true, amount: 0.8 }}>
-      <span className="animate-flash ">{'>'}</span>
-      {title}
-    </motion.div>
+    <div ref={ref} className="h-52 overflow-hidden">
+      <motion.div className="text-center text-[10rem] max-w-full h-fit text-wrap" style={{ y: yPos }} viewport={{ once: true }}>
+        <span className="animate-flash ">{'>'}</span>
+        {title}
+      </motion.div>
+    </div>
   );
 }
 
