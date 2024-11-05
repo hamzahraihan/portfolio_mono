68d586406573fd4f12ce41174eaca52f53b8443f
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-05 20:40:17 +0700

    add motion to navigation

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -2,6 +2,7 @@
 
 import { useTheme } from 'next-themes';
 import { Switch } from './ui/switch';
+import { motion } from 'framer-motion';
 
 function Navigation() {
   const { theme, setTheme } = useTheme();
@@ -20,11 +21,11 @@ function Navigation() {
   };
 
   return (
-    <div className="flex sticky top-0 justify-between items-center border-b dark:border-neutral-800 bg-neutral-50 dark:bg-neutral-900 text-sm h-12 mx-4">
+    <motion.div className="p-4 min-w-full flex justify-between items-center border-b dark:border-neutral-800 bg-neutral-50 dark:bg-neutral-900 text-sm h-12 transform-none" initial={{ y: 0 }}>
       <button onClick={() => scrollToSection('hero')}>hamzahraihan</button>
       <Switch onClick={() => setTheme(theme == 'dark' ? 'light' : 'dark')} checked={theme != 'light'} aria-readonly />
-      <div className="flex gap-20 h-full">
-        <div className="after:duration-200 ease-out after:block after:h-0.5 after:w-full after:origin-bottom-right after:scale-x-0 after:bg-neutral-900 after:dark:bg-neutral-300 after:transition-transform after:hover:origin-bottom-left after:hover:scale-x-100">
+      <div className="flex gap-20 h-12">
+        <div className="after:duration-200 ease-out after:block after:h-0.5 after:w-full after:origin-bottom-right after:scale-x-0 after:bg-neutral-900 after:dark:bg-neutral-300 after:transition-transform after:hover:origin-bottom-left after:hover:scale-x-100 h-full">
           <button className="flex h-full items-center" onClick={() => scrollToSection('what-i-do')}>
             <p>What i do</p>
           </button>
@@ -40,7 +41,7 @@ function Navigation() {
           </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
