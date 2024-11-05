908dbe32c8b80d0e300101715dea377b9efa263c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-05 15:47:22 +0700

    add about page

---
+++ src/app/components/about/about-me.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div id="about" className="text-center h-96">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat velit deserunt voluptas quaerat ea sunt non necessitatibus quos accusamus quo.</p>
+    </div>
+  );
+}
+
+export { AboutPage };
