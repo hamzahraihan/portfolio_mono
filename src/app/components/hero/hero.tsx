5b1386006cbedbb12d6631676856525588a6ac74
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-05 15:48:19 +0700

    refactor: change hero text from 'front-end' to 'fullstack' developer

--- src/app/components/hero/hero.tsx
+++ src/app/components/hero/hero.tsx
@@ -9,14 +9,14 @@ function HeadPage() {
   return (
     <div className="flex max-w-full h-[32rem] justify-evenly mt-10">
       <motion.div className="flex flex-col gap-5 w-96 mt-10" initial={{ opacity: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 1.5 }}>
-        <h1 className="text-2xl">Hi, my name is Hamzah Raihan I&apos;m a Front-End Developer</h1>
-        <p>a highly motivated developer with a passion for web development. I am currently seeking new opportunities to expand my skills and contribute to the field of technology.</p>
+        <h1 className="text-2xl">Hi, my name is Hamzah Raihan I&apos;m a Full-stack Developer</h1>
+        <p>a highly motivated developer with a passion for mobile and web development. I am currently seeking new opportunities to expand my skills and contribute to the field of technology.</p>
         <div className="h-10 w-10">
           <Scroll />
         </div>
       </motion.div>
       <motion.div className="w-fit" initial={{ opacity: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 2 }}>
-        <CircularText text="HAMZAHRAIHAN-SOFTWAREENGINEER-" className="motion-safe:animate-spin-slow duration-1000 dark:text-white" />
+        <CircularText text="HAMZAHRAIHAN-FULLSTACKDEVELOPER-MOBILE-WEB-" className="motion-safe:animate-spin-slow duration-1000 dark:text-white" />
       </motion.div>
     </div>
   );
