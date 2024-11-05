ce57965450451090d71a4c415abc70d7f95afeda
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-05 18:17:35 +0700

    refactor: change id for better seo

--- src/app/components/about/about-me.tsx
+++ src/app/components/about/about-me.tsx
@@ -1,10 +1,12 @@
+'use client';
+
 import React from 'react';
 
 function AboutPage() {
   return (
-    <div id="about" className="text-center h-96">
+    <section id="what-i-do" className="text-center h-96">
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat velit deserunt voluptas quaerat ea sunt non necessitatibus quos accusamus quo.</p>
-    </div>
+    </section>
   );
 }
 
