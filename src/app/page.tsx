61718f8692da319f7bb084ba3fcc9aaf819c7c1c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-05 11:21:38 +0700

    refactor: change import name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import { BigText } from './components/big_text';
+import { BigText } from './components/big-text';
 import { HeadPage } from './components/hero';
 
 export const metadata: Metadata = {
