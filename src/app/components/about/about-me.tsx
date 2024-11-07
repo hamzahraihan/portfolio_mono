ed41e81ed0ae690b3b101f8e2fb00eba98fa7e00
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-07 14:51:46 +0700

    enhance AboutPage with framer-motion for improved text animations

--- src/app/components/about/about-me.tsx
+++ src/app/components/about/about-me.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React from 'react';
+import { motion } from 'framer-motion';
 
 function AboutPage() {
   return (
@@ -10,12 +11,16 @@ function AboutPage() {
           <p>01</p> <div className="h-[0.10rem] w-5 bg-neutral-900 dark:bg-neutral-200 rounded-md" /> <p>WHAT I DO</p>
         </div>
         <div className="w-72 text-start">
-          <h1 className="pb-52 text-2xl">I transform complex ideas into elegant digital solutions through code and creativity.</h1>
-          <p className="text-sm pb-4">As a full-stack software engineer, I specialize in crafting seamless experiences across web and mobile platforms.</p>
-          <p className="text-sm">
+          <motion.h1 initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1, delay: 0.3 }} viewport={{ once: true }} className="pb-52 text-2xl">
+            I transform complex ideas into elegant digital solutions through code and creativity.
+          </motion.h1>
+          <motion.p initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1, delay: 0.6 }} viewport={{ once: true }} className="text-sm pb-4">
+            As a full-stack software engineer, I specialize in crafting seamless experiences across web and mobile platforms.
+          </motion.p>
+          <motion.p initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1, delay: 0.9 }} viewport={{ once: true }} className="text-sm">
             My expertise lies in building robust applications that combine intuitive design with powerful functionality. Every project I undertake is driven by a commitment to clean code, user-centered design, and innovative
             problem-solving.
-          </p>
+          </motion.p>
         </div>
       </div>
     </section>
