01098369bc2b268c61da789e550f97359de862ae
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-07 11:53:01 +0700

    refactor BigText component by removing unused scroll functionality for cleaner code

--- src/app/components/big-text/big-text.tsx
+++ src/app/components/big-text/big-text.tsx
@@ -1,21 +1,21 @@
 'use client';
 
-import { motion, useScroll, useTransform } from 'framer-motion';
-import { useRef } from 'react';
+import { motion } from 'framer-motion';
 
 function BigText({ title = '' }: Readonly<{ title: string }>) {
-  const ref = useRef<HTMLDivElement>(null);
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ['start end', 'center center'],
-  });
+  // const ref = useRef<HTMLDivElement>(null);
+  // const { scrollYProgress } = useScroll({
+  //   target: ref,
+  //   offset: ['start end', 'center center'],
+  // });
 
   // Transform scrollYProgress (0 to 1) into y position (100px to 0)
-  const yPos = useTransform(scrollYProgress, [0, 1], [200, 0]);
+  // const yPos = useTransform(scrollYProgress, [0, 1], [200, 0]);
+  // insert yPos to motion div with style
 
   return (
-    <div ref={ref} className="h-fit overflow-hidden">
-      <motion.div className="text-center text-6xl sm:text-7xl md:text-8xl lg:text-[10rem] max-w-full" style={{ y: yPos }}>
+    <div className="h-fit overflow-hidden border-b border-neutral-200 dark:border-neutral-600 pb-4">
+      <motion.div className="text-center text-6xl sm:text-7xl md:text-8xl lg:text-[10rem] max-w-full">
         <span className="animate-flash ">{'>'}</span>
         {title}
       </motion.div>
