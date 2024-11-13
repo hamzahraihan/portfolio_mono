5f5d369042eab10b8659a0381f2baef6aa18acd4
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-13 19:19:43 +0700

    add metadata and enhance AboutPage layout with animated text

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,22 @@
+import type { Metadata } from 'next';
 import React from 'react';
+import AnimatedText from './components/animated-text';
+
+export const metadata: Metadata = {
+  title: 'About - Hamzah Raihan',
+  description: 'about hamzah raihan',
+};
 
 function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <div className="container max-w-full my-16 h-[1000vh]">
+      <div className="text-[13rem] flex flex-wrap">
+        <div>WEB &</div>
+        <div>MOBILE DEVELOPER</div>
+      </div>
+      <AnimatedText />
+    </div>
+  );
 }
 
 export default AboutPage;
