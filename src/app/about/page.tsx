44bdd33db46b7518c81ddaec95e184f016504ffd
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-14 12:51:36 +0700

    add description section to AboutPage for enhanced content presentation

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,6 +11,9 @@ function AboutPage() {
   return (
     <div className="container max-w-full my-20 h-[1000vh]">
       <AnimatedText firstString="WEB &" secondString="MOBILE DEV" reverse={false} className="text-[32vh] font-bold inline-block whitespace-nowrap leading-[24vh]" />
+      <div className="flex justify-center items-center py-20 text-xs">
+        <div className="w-56 text-center uppercase text-neutral-500">Driven by a spirit of curiosity, his interests encompass language arts, literature, visual design, film and music.</div>
+      </div>
       <AnimatedText firstString="BASED IN" secondString="INDONESIA" reverse={true} className="text-[32vh] font-bold inline-block whitespace-nowrap leading-[24vh]" />
     </div>
   );
