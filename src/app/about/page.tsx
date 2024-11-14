14141d8631968a028050171e232eb2bb1f370ef8
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-14 16:08:27 +0700

    refactor AboutPage layout to improve spacing and animation transitions

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,22 +12,24 @@ function AboutPage() {
   return (
     <div className="container max-w-full">
       <div className="flex justify-center items-center h-screen">
-        <div className="flex flex-col h-96">
+        <div className="flex flex-col h-96 gap-2">
           <div className="flex justify-between text-xs uppercase">
             <p>web</p>
             <p className="text-neutral-400">( frontend developer )</p>
             <p>mobile</p>
           </div>
-          <div className="bg-neutral-400 h-96 w-72 overflow-hidden blur grayscale hover:blur-0 transition-all duration-150 ">
-            <Image className="object-cover translate-y-10 w-full h-full mix-blend-multiply ease-in-out" src="/icepop.png" alt="icepop guy" width={500} height={500} />
+          <div className="bg-neutral-400 h-96 w-72 overflow-hidden blur grayscale hover:blur-0 transition-all duration-300 ease-in-out">
+            <Image className="object-cover translate-y-10 w-full h-full mix-blend-multiply" src="/icepop.png" alt="icepop guy" width={500} height={500} />
           </div>
         </div>
       </div>
-      <AnimatedText firstString="WEB &" secondString="MOBILE DEV" reverse={false} className="text-[32vh] font-bold inline-block whitespace-nowrap leading-[24vh]" />
-      <div className="flex justify-center items-center py-20 text-xs">
-        <div className="w-56 text-center uppercase text-neutral-500">Driven by a spirit of curiosity, his interests encompass language arts, literature, visual design, film and music.</div>
+      <div className="h-[200vh]">
+        <AnimatedText firstString="WEB &" secondString="MOBILE DEV" reverse={false} className="text-[32vh] font-bold inline-block whitespace-nowrap leading-[24vh]" />
+        <div className="flex justify-center items-center py-20 text-xs">
+          <div className="w-56 text-center uppercase text-neutral-500">Driven by a spirit of curiosity, his interests encompass language arts, literature, visual design, film and music.</div>
+        </div>
+        <AnimatedText firstString="BASED IN" secondString="INDONESIA" reverse={true} className="text-[32vh] font-bold inline-block whitespace-nowrap leading-[24vh]" />
       </div>
-      <AnimatedText firstString="BASED IN" secondString="INDONESIA" reverse={true} className="text-[32vh] font-bold inline-block whitespace-nowrap leading-[24vh]" />
     </div>
   );
 }
