9cc12a28e6efc3acdbc61e365d0a99f76eac9fe0
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-14 08:32:10 +0700

    refactor AnimatedText component to accept props for dynamic strings and reverse functionality, enhancing animation handling and layout

--- src/app/about/components/animated-text.tsx
+++ src/app/about/components/animated-text.tsx
@@ -4,55 +4,52 @@
 import { useScroll, useTransform, motion, useSpring } from 'framer-motion';
 import React, { useRef } from 'react';
 
-const AnimatedText = () => {
+const AnimatedText = ({ firstString, secondString, reverse, className }: { firstString: string; secondString: string; reverse: boolean; className: string }) => {
   const containerRef = useRef(null);
   const { scrollYProgress } = useScroll({
     target: containerRef,
     offset: ['center center', 'start end'],
   });
 
-  const firstString = 'BASED IN';
-  const secondString = 'INDONESIA';
-
   const firstLetters = firstString.split('');
   const secondLetters = secondString.split('');
 
-  const handleAnimatedText = (letters: string[]) => {
+  const HandleAnimatedText = (letters: string[], i: number) => {
     const totalLetters = letters.length;
-    const opacityArray = letters.map((_, i) => {
-      const reverseIndex = totalLetters - 1 - i;
+    const handleReverseIndex = () => {
+      if (reverse) {
+        return totalLetters - 1 - i;
+      }
+      if (!reverse) {
+        return i;
+      }
+    };
+    const reverseIndex = handleReverseIndex() ?? 0;
+    const opacityArray = useTransform(scrollYProgress, [reverseIndex * 0.02, (reverseIndex + 1) * 0.05], [1, 0], { clamp: true });
 
-      return useTransform(scrollYProgress, [reverseIndex * 0.02, (reverseIndex + 1) * 0.05], [1, 0], { clamp: false });
-    });
-
-    const yArray = letters.map((_, i) => {
-      const reverseIndex = totalLetters - 1 - i;
-      return useTransform(scrollYProgress, [reverseIndex * 0.02, (reverseIndex + 1) * 0.05], [0, 210]);
-    });
+    const yArray = useTransform(scrollYProgress, [reverseIndex * 0.02, (reverseIndex + 1) * 0.05], [0, 210], { clamp: true });
 
     const physics = { damping: 15, mass: 0.1, stiffness: 200 };
-    const springArray = yArray.map((y) => useSpring(y, physics));
+    const springArray = useSpring(yArray, physics);
     return { opacityArray, springArray };
   };
 
   return (
-    <div ref={containerRef} className="container max-w-full flex justify-end px-6  flex-col">
-      <div className="flex flex-wrap justify-end">
+    <div ref={containerRef} className="container max-w-full h-fit flex justify-end px-6 flex-col">
+      <div className={`flex flex-wrap ${!reverse ? 'justify-start' : 'justify-end'}`}>
         {firstLetters.map((letter, i) => (
           <div key={`${letter}-${i}`}>
-            <motion.span className="text-[17rem] font-bold flex justify-end h-64" style={{ opacity: handleAnimatedText(firstLetters).opacityArray[i], y: handleAnimatedText(firstLetters).springArray[i] }}>
+            <motion.span className={className} style={{ opacity: HandleAnimatedText(firstLetters, i).opacityArray, y: HandleAnimatedText(firstLetters, i).springArray }}>
               {letter === ' ' ? '\u00A0' : letter}
             </motion.span>
           </div>
         ))}
       </div>
-      <div className="flex flex-wrap justify-end">
+      <div className={`flex flex-wrap ${!reverse ? 'justify-start' : 'justify-end'}`}>
         {secondLetters.map((letter, i) => (
-          <div key={`${letter}-${i}`}>
-            <motion.span className="text-[17rem] font-bold flex justify-end h-64" style={{ opacity: handleAnimatedText(secondLetters).opacityArray[i], y: handleAnimatedText(secondLetters).springArray[i] }}>
-              {letter === ' ' ? '\u00A0' : letter}
-            </motion.span>
-          </div>
+          <motion.span key={`${letter}-${i}`} className={className} style={{ opacity: HandleAnimatedText(secondLetters, i).opacityArray, y: HandleAnimatedText(secondLetters, i).springArray }}>
+            {letter === ' ' ? '\u00A0' : letter}
+          </motion.span>
         ))}
       </div>
     </div>
