17e7b34248458f3ef68c757302f2cb3e78458246
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-15 14:49:54 +0700

    add IntroductionSection component to AboutPage for enhanced content presentation

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import React from 'react';
 import AnimatedText from './components/animated-text';
 import Image from 'next/image';
+import IntroductionSection from './components/introduction';
 
 export const metadata: Metadata = {
   title: 'About - Hamzah Raihan',
@@ -23,13 +24,15 @@ function AboutPage() {
           </div>
         </div>
       </div>
-      <div className="h-[200vh]">
+      <div className="h-[150vh]">
         <AnimatedText firstString="WEB &" secondString="MOBILE DEV" reverse={false} className="text-[32vh] font-bold inline-block whitespace-nowrap leading-[24vh]" />
         <div className="flex justify-center items-center py-20 text-xs">
           <div className="w-56 text-center uppercase text-neutral-500">Driven by a spirit of curiosity, his interests encompass language arts, literature, visual design, film and music.</div>
         </div>
         <AnimatedText firstString="BASED IN" secondString="INDONESIA" reverse={true} className="text-[32vh] font-bold inline-block whitespace-nowrap leading-[24vh]" />
       </div>
+
+      <IntroductionSection />
     </div>
   );
 }
