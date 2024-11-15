943dd09ce7bc923160e5078803c86acc7a386378
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-15 16:26:12 +0700

    enhance IntroductionSection component to support dynamic positioning and optional links

--- src/app/about/components/animated-about.tsx
+++ src/app/about/components/animated-about.tsx
@@ -3,22 +3,35 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-function IntroductionSection({ texts, title, id }: Readonly<{ texts: string[]; title: string; id: number }>) {
+type SectionProps = {
+  texts: string[];
+  title: string;
+  id: number;
+  position: 'left' | 'right';
+  links?: Array<{ title: string; link: string }>;
+};
+
+function IntroductionSection({ texts, title, id, position, links }: Readonly<SectionProps>) {
   return (
-    <div className="max-w-full px-10 py-5">
-      <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1, delay: 0.5 }} viewport={{ once: true }} className="h-fit w-[40vw] border-t border-l border-neutral-500 p-3 lowercase">
-        <h1 className="pb-2 flex items-center gap-1">
-          {id} <div className="w-2 h-0.5 rounded-lg bg-white" /> {title}
-        </h1>
+    <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1, delay: 0.3 }} viewport={{ once: true }} className={`h-fit w-[40vw] px-10 py-5 ${position == 'right' ? '' : 'ml-auto'} `}>
+      <h1 className="pb-2 flex items-center gap-1">
+        {id} <div className="w-2 h-0.5 rounded-lg bg-white" /> {title}
+      </h1>
+      <div className="border-t border-l border-neutral-500 p-3 lowercase">
         <div className="flex flex-col gap-2">
           {texts.map((text, i) => (
-            <p key={i + 2} className="text-[2vh]">
-              {text}
-            </p>
+            <div key={i + 2} className="text-[2vh]">
+              {text}{' '}
+              <span>
+                <a href={links?.[i]?.link} target="_blank" rel="noreferrer noopener" className="text-neutral-700 dark:text-neutral-500 hover:text-neutral-900 dark:hover:text-neutral-100 duration-100">
+                  {links?.[i]?.title}
+                </a>
+              </span>
+            </div>
           ))}
         </div>
-      </motion.div>
-    </div>
+      </div>
+    </motion.div>
   );
 }
 
