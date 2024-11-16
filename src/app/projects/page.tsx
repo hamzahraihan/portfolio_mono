e410ccf7dc99b79f5bca1cb205b8f1ae87c53853
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-16 15:50:40 +0700

    feat: update layout of ProjectPage for improved responsiveness

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -9,15 +9,10 @@ export const metadata: Metadata = {
 
 function ProjectPage() {
   return (
-    <div className="container mx-auto max-w-4xl my-20">
-      <ProjectHeadSection />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi enim magnam reprehenderit dolorem atque id totam tempore voluptas, commodi consequatur dolore harum quasi facere aliquid quas laboriosam vel maxime voluptatem nemo
-      sequi doloremque quo? Veritatis molestias quaerat amet architecto odit, reiciendis delectus aperiam fugiat qui repellendus praesentium reprehenderit dignissimos dolorum vero soluta quisquam neque. Deleniti, cum? Illo alias recusandae
-      quia asperiores omnis ducimus impedit illum sapiente autem ab inventore eaque dicta, odio earum repellat voluptatibus cumque! Architecto doloremque ipsam suscipit dicta. Necessitatibus eveniet labore ratione veritatis qui dolor neque
-      excepturi adipisci explicabo, ipsam sint mollitia! Quia deserunt nostrum fugit ab commodi nemo obcaecati, placeat fugiat velit illum ipsa quo rerum error adipisci praesentium pariatur! Nemo maiores, tempora cum quibusdam sed voluptas
-      rerum repudiandae doloribus ratione omnis. Exercitationem maiores vel sed earum architecto molestias labore ut dolore quae in. Asperiores voluptatum adipisci mollitia molestias quasi a corrupti vero tempore dicta magni saepe, nihil
-      dolores quisquam quod, id placeat architecto, veritatis cupiditate magnam repellendus. Quo nulla, adipisci delectus deserunt aliquam dolor reiciendis recusandae obcaecati, ab, fuga maiores magni. Repellendus magnam corrupti voluptatem
-      quasi nam. Architecto veniam ea aliquam sint ut accusantium laboriosam modi facere cum, voluptatem earum excepturi temporibus, aperiam incidunt atque.
+    <div className="container mx-auto max-w-full my-20">
+      <div className="max-w-7xl mx-auto px-2">
+        <ProjectHeadSection />
+      </div>
     </div>
   );
 }
