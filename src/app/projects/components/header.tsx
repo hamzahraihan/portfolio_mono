975f24f00b9db75875f4e2210ed61a3d5e8e0220
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-17 14:51:46 +0700

    feat: add padding and margin to ProjectHeadSection for improved layout

--- src/app/projects/components/header.tsx
+++ src/app/projects/components/header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ProjectHeadSection() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col p-10 mb-10">
       <h1 className="text-5xl font-bold">Projects</h1>
       <h1 className="pb-2 flex items-center gap-1">
         <div className="w-5 h-0.5 rounded-lg bg-white" /> selected projects
