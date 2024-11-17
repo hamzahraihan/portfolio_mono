582856f08fef3f3b39d468495e659b135fa224f2
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-17 11:26:31 +0700

    feat: add ProjectCard component to ProjectPage for enhanced project display

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import React from 'react';
 import ProjectHeadSection from './components/header';
+import ProjectCard from './components/card';
 
 export const metadata: Metadata = {
   title: 'Projects - Hamzah Raihan',
@@ -12,6 +13,7 @@ function ProjectPage() {
     <div className="container mx-auto max-w-full my-20">
       <div className="max-w-5xl mx-auto px-2">
         <ProjectHeadSection />
+        <ProjectCard />
       </div>
     </div>
   );
