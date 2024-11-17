c55481d6dacf505cd28b834ecdb2a4ada5ae142e
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-17 11:26:34 +0700

    feat: enhance ProjectCard layout with improved styling and grid structure

--- src/app/projects/components/card.tsx
+++ src/app/projects/components/card.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function ProjectCard() {
-  return <div>ProjectCard</div>;
+  return (
+    <div className="border-t border-l border-neutral-500 p-2">
+      <div className="grid grid-cols-3 gap-4">
+        <div>Weather App</div>
+        <div className="text-center">Weather App</div>
+        <div>Weather App</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProjectCard;
