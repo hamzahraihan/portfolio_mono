d58220f31febdd3045023a6d328b43eee0144b50
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-17 14:51:37 +0700

    feat: enhance ProjectCard for Weather App with serviceProvide, application, and link props

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -14,9 +14,12 @@ function ProjectPage() {
       <div className="max-w-7xl mx-auto px-2">
         <ProjectHeadSection />
         <ProjectCard
+          serviceProvide={['frontend developer']}
           title="Weather App"
+          application="web"
           description="Weather app is your go-to source for accurate and up-to-date weather information. With a sleek and intuitive interface, you can easily check the current conditions, hourly forecasts, and 7-day outlooks for your desired location."
           tech={['Typescript', 'React JS', 'Geopify API', 'Open Mateo API', 'Open Weather API']}
+          link="javascript:(0)"
         />
       </div>
     </div>
