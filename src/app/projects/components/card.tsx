6eb398f0b66b8251076494397a8d3b3aab0e5268
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-17 14:17:02 +0700

    feat: refactor ProjectCard component to accept dynamic title and description props

--- src/app/projects/components/card.tsx
+++ src/app/projects/components/card.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
-function ProjectCard() {
+type ProjectProps = {
+  description: string;
+  title: string;
+  tech?: string[];
+  image?: string;
+};
+
+function ProjectCard({ description, title, tech, image }: Readonly<ProjectProps>) {
   return (
     <div className="border-t border-l border-neutral-500 p-2">
       <div className="grid grid-cols-3 gap-4">
-        <div>Weather App</div>
-        <div className="text-center">Weather App</div>
-        <div>Weather App</div>
+        <div>{title}</div>
+        <div className="mx-auto text-start text-sm w-60">{description}</div>
+        <div className="flex justify-end">image</div>
       </div>
     </div>
   );
