c1be88b3f24869aa9f4be9efe169cf3a0feee1ab
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-18 16:20:49 +0700

    feat: add Layout component for project pages with metadata and structured layout

---
+++ src/app/projects/layout.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Projects - Hamzah Raihan',
+  description: 'portfolio website',
+};
+
+type LayoutProps = {
+  children: React.ReactNode;
+  project: React.ReactNode;
+};
+
+export default function Layout({ children, project }: Readonly<LayoutProps>) {
+  return (
+    <div className="container mx-auto max-w-full my-20">
+      <div>{children}</div>
+      <div className="container max-w-full px-10 flex gap-2 justify-center items-center">{project}</div>
+    </div>
+  );
+}
