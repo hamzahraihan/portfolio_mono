b92ae6100cd62dd46dda805d42bdb3300c24737c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-19 15:46:09 +0700

    feat: add tags to revalidation options in dataFetcher for project data caching

--- src/services/dataFetcher.tsx
+++ src/services/dataFetcher.tsx
@@ -1,7 +1,7 @@
 export const getData = async (url: string) => {
   const res = await fetch(url, {
     cache: 'force-cache',
-    next: { revalidate: 30 },
+    next: { revalidate: 30, tags: ['projects'] },
   });
   if (!res.ok) throw new Error('Failed to fetch data');
   return res.json();
