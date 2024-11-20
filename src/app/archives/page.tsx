009c3664c7d3110be1b863295cca2ea91a279a94
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-20 21:56:10 +0700

    feat: remove HorizontalScroll component from ArchivePage for simplified layout

--- src/app/archives/page.tsx
+++ src/app/archives/page.tsx
@@ -1,22 +1,16 @@
-import HorizontalScroll from '@/components/horizontal-scroll';
 import React from 'react';
 
 function ArchivePage() {
   return (
-    <>
-      <h1 className="text-center text-neutral-400">( archives )</h1>
-      <HorizontalScroll>
-        <div className="relative flex ">
-          <div className="w-[500px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestias cupiditate obcaecati quia omnis aspernatur deserunt voluptatem consectetur rem tenetur.</div>
-          <div className="w-[500px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestias cupiditate obcaecati quia omnis aspernatur deserunt voluptatem consectetur rem tenetur.</div>
-          <div className="w-[500px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestias cupiditate obcaecati quia omnis aspernatur deserunt voluptatem consectetur rem tenetur.</div>
-          <div className="w-[500px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestias cupiditate obcaecati quia omnis aspernatur deserunt voluptatem consectetur rem tenetur.</div>
-          <div className="w-[500px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestias cupiditate obcaecati quia omnis aspernatur deserunt voluptatem consectetur rem tenetur.</div>
-          <div className="w-[500px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestias cupiditate obcaecati quia omnis aspernatur deserunt voluptatem consectetur rem tenetur.</div>
-          <div className="w-[500px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestias cupiditate obcaecati quia omnis aspernatur deserunt voluptatem consectetur rem tenetur.</div>
-        </div>
-      </HorizontalScroll>
-    </>
+    <div className="relative flex">
+      <div className="w-[500px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestias cupiditate obcaecati quia omnis aspernatur deserunt voluptatem consectetur rem tenetur.</div>
+      <div className="w-[500px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestias cupiditate obcaecati quia omnis aspernatur deserunt voluptatem consectetur rem tenetur.</div>
+      <div className="w-[500px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestias cupiditate obcaecati quia omnis aspernatur deserunt voluptatem consectetur rem tenetur.</div>
+      <div className="w-[500px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestias cupiditate obcaecati quia omnis aspernatur deserunt voluptatem consectetur rem tenetur.</div>
+      <div className="w-[500px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestias cupiditate obcaecati quia omnis aspernatur deserunt voluptatem consectetur rem tenetur.</div>
+      <div className="w-[500px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestias cupiditate obcaecati quia omnis aspernatur deserunt voluptatem consectetur rem tenetur.</div>
+      <div className="w-[500px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestias cupiditate obcaecati quia omnis aspernatur deserunt voluptatem consectetur rem tenetur.</div>
+    </div>
   );
 }
 
