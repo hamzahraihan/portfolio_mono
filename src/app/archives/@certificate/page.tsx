e6c4f03b74ade7c6c1e1af64d1d3fba1933152f1
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-20 15:53:34 +0700

    feat: add CertificateSection component for displaying certificate content

---
+++ src/app/archives/@certificate/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CertificateSection() {
+  return (
+    <div className="">
+      <h1>certificate</h1>
+    </div>
+  );
+}
+
+export default CertificateSection;
