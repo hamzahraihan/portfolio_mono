bb45b4374ad0bfdbb05a40fb9b5a40dd7ecb6d3c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-20 22:06:43 +0700

    feat: add fade-in animation to ArchivePage using framer-motion for improved visual transition

--- src/app/archives/page.tsx
+++ src/app/archives/page.tsx
@@ -1,16 +1,25 @@
+'use client';
+
+import { AnimatePresence, motion } from 'framer-motion';
 import React from 'react';
 
 function ArchivePage() {
+  const fadeVariant = {
+    hidden: { opacity: 0, filter: 'blur(5px)' },
+    visible: { opacity: 1, filter: 'none' },
+    exit: { opacity: 0, filter: 'blur(5px)' },
+  };
+
   return (
-    <div className="relative flex">
-      <div className="w-[500px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestias cupiditate obcaecati quia omnis aspernatur deserunt voluptatem consectetur rem tenetur.</div>
-      <div className="w-[500px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestias cupiditate obcaecati quia omnis aspernatur deserunt voluptatem consectetur rem tenetur.</div>
-      <div className="w-[500px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestias cupiditate obcaecati quia omnis aspernatur deserunt voluptatem consectetur rem tenetur.</div>
-      <div className="w-[500px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestias cupiditate obcaecati quia omnis aspernatur deserunt voluptatem consectetur rem tenetur.</div>
-      <div className="w-[500px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestias cupiditate obcaecati quia omnis aspernatur deserunt voluptatem consectetur rem tenetur.</div>
-      <div className="w-[500px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestias cupiditate obcaecati quia omnis aspernatur deserunt voluptatem consectetur rem tenetur.</div>
-      <div className="w-[500px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestias cupiditate obcaecati quia omnis aspernatur deserunt voluptatem consectetur rem tenetur.</div>
-    </div>
+    <AnimatePresence mode="popLayout">
+      <div className="relative flex">
+        {Array.from({ length: 7 }).map((_, index) => (
+          <motion.div key={index + 1} className="w-[500px]" variants={fadeVariant} initial="hidden" animate="visible" exit="exit" transition={{ ease: 'easeInOut', duration: 0.75 }}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestias cupiditate obcaecati quia omnis aspernatur deserunt voluptatem consectetur rem tenetur.
+          </motion.div>
+        ))}
+      </div>
+    </AnimatePresence>
   );
 }
 
