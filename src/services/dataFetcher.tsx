34f05381787a757c44e188a6a162bc2145c0b0e4
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-20 18:49:44 +0700

    refactor: remove force-cache option from data fetching

--- src/services/dataFetcher.tsx
+++ src/services/dataFetcher.tsx
@@ -1,6 +1,5 @@
 export const getData = async (url: string) => {
   const res = await fetch(url, {
-    cache: 'force-cache',
     next: { revalidate: 30, tags: ['projects'] },
   });
   if (!res.ok) throw new Error('Failed to fetch data');
