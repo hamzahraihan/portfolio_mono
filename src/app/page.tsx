2899d4941e282c5b78a72ce10227e502aeccb49b
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-20 22:06:40 +0700

    feat: implement fade-in animation for Home component using framer-motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,28 @@
-import type { Metadata } from 'next';
+'use client';
+
 import { BigText } from './components/big-text';
 import { HeroPage } from './components/hero';
 import { AboutPage } from './components/about';
 import { OtherProjectCTA, MyServicePage } from './components/my-services';
-
-export const metadata: Metadata = {
-  title: 'Hamzah Raihan',
-  description: 'portfolio website',
-};
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function Home() {
+  const fadeVariant = {
+    hidden: { opacity: 0, filter: 'blur(5px)' },
+    visible: { opacity: 1, filter: 'none' },
+    exit: { opacity: 0, filter: 'blur(5px)' },
+  };
   return (
     <main className="max-w-full h-full">
-      <HeroPage />
-      <BigText title="software_engineer" />
-      <AboutPage />
-      <MyServicePage />
-      <OtherProjectCTA />
+      <AnimatePresence mode="popLayout">
+        <motion.section variants={fadeVariant} initial="hidden" animate="visible" exit="exit" transition={{ ease: 'easeInOut', duration: 0.75 }}>
+          <HeroPage />
+          <BigText title="software_engineer" />
+          <AboutPage />
+          <MyServicePage />
+          <OtherProjectCTA />
+        </motion.section>
+      </AnimatePresence>
     </main>
   );
 }
