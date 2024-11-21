aeffe60f1a02df6f737a32e008c09e3499b24d65
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-21 15:50:57 +0700

    refactor: remove AnimatedLayout wrapper from Archives layout for simplified structure

--- src/app/archives/layout.tsx
+++ src/app/archives/layout.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import type { Metadata } from 'next';
 import HorizontalScroll from '@/components/horizontal-scroll';
-import AnimatedLayout from '@/components/animated-presence';
 
 export const metadata: Metadata = {
   title: 'Archives - Hamzah Raihan',
@@ -14,11 +13,9 @@ type LayoutProps = {
 
 export default function Layout({ children }: Readonly<LayoutProps>) {
   return (
-    <AnimatedLayout>
-      <div className="overscroll-none overflow-hidden">
-        <h1 className="text-center text-neutral-400">( archives )</h1>
-        <HorizontalScroll>{children}</HorizontalScroll>
-      </div>
-    </AnimatedLayout>
+    <div className="overscroll-none overflow-hidden">
+      <h1 className="text-center text-neutral-400">( archives )</h1>
+      <HorizontalScroll>{children}</HorizontalScroll>
+    </div>
   );
 }
