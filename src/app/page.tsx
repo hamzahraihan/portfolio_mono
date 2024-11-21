a9f710dca461f0e71c313efa7fbcda67f1a60455
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-21 15:50:51 +0700

    feat: wrap main content in AnimatedLayout for improved animation effects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,18 @@ import { BigText } from './components/big-text';
 import { HeroPage } from './components/hero';
 import { AboutPage } from './components/about';
 import { OtherProjectCTA, MyServicePage } from './components/my-services';
+import AnimatedLayout from '@/components/animated-presence';
 
 export default function Home() {
   return (
     <main className="max-w-full h-full">
-      <HeroPage />
-      <BigText title="software_engineer" />
-      <AboutPage />
-      <MyServicePage />
-      <OtherProjectCTA />
+      <AnimatedLayout>
+        <HeroPage />
+        <BigText title="software_engineer" />
+        <AboutPage />
+        <MyServicePage />
+        <OtherProjectCTA />
+      </AnimatedLayout>
     </main>
   );
 }
