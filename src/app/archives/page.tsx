60abda6598315ae869265c49f911c2ee5330b028
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-21 14:57:28 +0700

    feat: replace framer-motion animations with CertificateSection for simplified ArchivePage layout

--- src/app/archives/page.tsx
+++ src/app/archives/page.tsx
@@ -1,25 +1,19 @@
-'use client';
-
-import { AnimatePresence, motion } from 'framer-motion';
 import React from 'react';
+import CertificateSection from './_components/certificate';
 
 function ArchivePage() {
-  const fadeVariant = {
-    hidden: { opacity: 0, filter: 'blur(5px)' },
-    visible: { opacity: 1, filter: 'none' },
-    exit: { opacity: 0, filter: 'blur(5px)' },
-  };
-
   return (
-    <AnimatePresence mode="popLayout">
-      <div className="relative flex">
-        {Array.from({ length: 7 }).map((_, index) => (
-          <motion.div key={index + 1} className="w-[500px]" variants={fadeVariant} initial="hidden" animate="visible" exit="exit" transition={{ ease: 'easeInOut', duration: 0.75 }}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestias cupiditate obcaecati quia omnis aspernatur deserunt voluptatem consectetur rem tenetur.
-          </motion.div>
-        ))}
+    <>
+      <div className="h-screen w-screen">
+        <CertificateSection />
       </div>
-    </AnimatePresence>
+      <div className="h-screen w-screen">
+        <CertificateSection />
+      </div>
+      <div className="h-screen w-screen">
+        <CertificateSection />
+      </div>
+    </>
   );
 }
 
