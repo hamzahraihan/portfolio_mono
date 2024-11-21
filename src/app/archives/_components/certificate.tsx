7e1f763dd34937635d7930713d798771e597b2b1
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-21 15:51:08 +0700

    feat: update CertificateSection layout with padding and additional content

--- src/app/archives/_components/certificate.tsx
+++ src/app/archives/_components/certificate.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function CertificateSection() {
-  return <div className="text-center h-full w-full">CertificateSection</div>;
+  return (
+    <div className="text-center h-full w-full p-10">
+      <div>certificate</div>
+    </div>
+  );
 }
 
 export default CertificateSection;
