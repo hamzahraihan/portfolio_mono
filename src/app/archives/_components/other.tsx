3b9c31e42159031eacfadb9c6b661d9ee6ed5ffd
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-22 17:17:53 +0700

    feat: add OtherSection component to archives

---
+++ src/app/archives/_components/other.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function OtherSection() {
+  return (
+    <div className="text-center h-full w-full p-10">
+      <div>Other noteworthy projects</div>
+    </div>
+  );
+}
+
+export default OtherSection;
