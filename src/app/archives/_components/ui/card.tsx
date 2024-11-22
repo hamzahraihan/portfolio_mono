aef21bb9bdc3a1a4241666a474fc83e3fff24475
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-22 14:49:52 +0700

    feat: add ArchiveCard component for displaying archive entries

---
+++ src/app/archives/_components/ui/card.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+type CardProps = {
+  row: string;
+  span: string;
+  col?: string;
+  title: string;
+  date: string;
+  position: string;
+  location: string;
+  description: string;
+};
+
+function ArchiveCard({ row, span, col, title, date, position, location, description }: Readonly<CardProps>) {
+  return (
+    <div className={`flex flex-col text-sm ${row} ${span} ${col}`}>
+      <div className="flex justify-between">
+        <p>{title}</p>
+        <p className="text-neutral-400">{date}</p>
+      </div>
+      <div className="flex flex-col gap-4 border-t border-l border-neutral-500 p-2">
+        <div className="container">
+          <div className="flex justify-between text-md">
+            <p>{position}</p>
+            <p className="text-neutral-400">{location}</p>
+          </div>
+        </div>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ArchiveCard;
