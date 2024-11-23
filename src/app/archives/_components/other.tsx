4a91a9f1fed4364949a11d9c39281e45086776b0
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-23 15:20:09 +0700

    feat: enhance layout of OtherSection component in archives

--- src/app/archives/_components/other.tsx
+++ src/app/archives/_components/other.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 function OtherSection() {
   return (
-    <div className="text-center h-full w-full p-10">
-      <div>Other noteworthy projects</div>
+    <div className="h-full w-full p-10">
+      <div className="flex flex-col justify-between w-full h-full">
+        <h1 className="h-fit w-fit text-8xl">Other noteworthy projects</h1>
+        <div className="h-96 w-full"></div>
+      </div>
     </div>
   );
 }
