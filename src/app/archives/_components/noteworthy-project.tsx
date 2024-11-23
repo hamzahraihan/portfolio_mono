c4a84c225f974716291fbaf23db893f875a78ddf
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-23 16:44:32 +0700

    feat: add NoteworthySection component to archives

---
+++ src/app/archives/_components/noteworthy-project.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react';
+
+function NoteworthySection() {
+  return (
+    <div className="h-full w-full py-10 px-2">
+      <div className="flex flex-col justify-between w-full h-full">
+        <h1 className="h-fit w-fit text-8xl">Other noteworthy projects</h1>
+        <div className=" h-96 w-full"></div>
+      </div>
+    </div>
+  );
+}
+
+export default NoteworthySection;
