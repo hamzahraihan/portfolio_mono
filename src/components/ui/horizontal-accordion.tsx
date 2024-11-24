c56fef32e37fac6d182ff3a95b0f13d396f554e8
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-24 15:08:23 +0700

    feat: refactor HorizontalAccordion layout for improved responsiveness and clarity

--- src/components/ui/horizontal-accordion.tsx
+++ src/components/ui/horizontal-accordion.tsx
@@ -38,33 +38,27 @@ const items = [
 function HorizontalAccordion() {
   const [activeTab, setActiveTab] = useState(0);
   return (
-    <div className="flex w-full h-96 gap-2 p-4">
+    <div className="flex w-full h-[60vh] gap-2">
       {items.map((item, index) => (
         <button
           type="button"
           key={index + 1}
-          className={`relative overflow-hidden rounded-lg transition-all duration-500 ease-in-out cursor-pointer
+          className={`border-l relative overflow-hidden transition-all duration-500 ease-in-out
         ${activeTab === index ? 'flex-grow' : 'flex-grow-0 hover:flex-grow-[0.1]'}
-        ${activeTab === index ? 'w-96' : 'w-24'}`}
+        ${activeTab === index ? 'w-96' : 'w-12'}`}
           onClick={() => setActiveTab(index)}
         >
-          <div className="absolute inset-0 bg-black bg-opacity-40">
+          <div className="flex h-full bg-transparent">
+            <div className="flex flex-col justify-between w-12 transition-opacity duration-300">
+              <h3 className="font-semibold transform -rotate-90 text-xs">{item.title}</h3>
+              <h4 className="transform -rotate-90 text-xs">2023 / 2024</h4>
+            </div>
             <div
               className={`h-full flex flex-col justify-between p-4 text-white transition-opacity duration-300
           ${activeTab === index ? 'opacity-100' : 'opacity-0'}`}
             >
               <h3 className="text-xl font-semibold">{item.title}</h3>
               <p className="text-sm">{item.description}</p>
-            </div>
-            <div
-              className={`absolute inset-0 flex items-center transition-opacity duration-300
-          ${activeTab === index ? 'opacity-0' : 'opacity-100'}`}
-            >
-              <div className='text-white font-semibold transform -rotate-90 whitespace-nowrap"'>
-                <h3>{item.title}</h3>
-
-                <h4>2023 / 2024</h4>
-              </div>
             </div>
           </div>
         </button>
