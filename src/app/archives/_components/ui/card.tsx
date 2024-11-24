38f4b5cb035d9eb7715f5e7d12b9fe0f19df1a75
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-24 17:22:18 +0700

    feat: update ArchiveCard font size for improved readability

--- src/app/archives/_components/ui/card.tsx
+++ src/app/archives/_components/ui/card.tsx
@@ -13,7 +13,7 @@ type CardProps = {
 
 function ArchiveCard({ row, span, col, title, date, position, location, description }: Readonly<CardProps>) {
   return (
-    <div className={`flex flex-col text-sm ${row} ${span} ${col}`}>
+    <div className={`flex flex-col text-[2vh] ${row} ${span} ${col}`}>
       <div className="flex justify-between">
         <p>{title}</p>
         <p className="text-neutral-400">{date}</p>
