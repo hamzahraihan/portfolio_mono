9e5cadbf4e0f55d52c93b326e912b0756dd9d19f
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-28 16:31:20 +0700

    fix: adjust font size for NoteworthySection heading for improved responsiveness

--- src/app/archives/_components/noteworthy-project.tsx
+++ src/app/archives/_components/noteworthy-project.tsx
@@ -6,7 +6,7 @@ function NoteworthySection() {
   return (
     <div className="h-full w-full py-10 px-2">
       <div className="flex flex-col justify-between w-full h-full">
-        <h1 className="h-fit w-fit text-[12vh]">Other noteworthy projects</h1>
+        <h1 className="h-fit w-fit text-[8vh] md:text-[9vh] lg:text-[10vh]">Other noteworthy projects</h1>
         <div className="w-full">
           <HorizontalAccordion />
         </div>
