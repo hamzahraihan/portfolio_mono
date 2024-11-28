b3e186519a6e638721e7d52ca09a268d33f95290
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-28 14:50:04 +0700

    feat: implement HorizontalScroll for improved layout on larger screens

--- src/app/archives/page.tsx
+++ src/app/archives/page.tsx
@@ -1,26 +1,36 @@
 'use client';
 
-import React from 'react';
 import NoteworthySection from './_components/noteworthy-project';
 import AnimatedLayout from '@/components/animated-presence';
 import JourneySection from './_components/journey';
 import AchievementSection from './_components/achievements';
 
+import HorizontalScroll from '@/components/horizontal-scroll';
 function ArchivePage() {
   return (
-    <AnimatedLayout>
-      <div className="relative flex">
-        <div className="h-screen w-screen">
-          <JourneySection />
-        </div>
-        <div className="h-screen w-screen">
-          <NoteworthySection />
-        </div>
-        <div className="h-screen w-screen">
-          <AchievementSection />
-        </div>
+    <>
+      {/* web view */}
+      <div className="hidden lg:block">
+        <HorizontalScroll>
+          <AnimatedLayout>
+            <div className="relative flex">
+              <div className="h-screen w-screen">
+                <JourneySection />
+              </div>
+              <div className="h-screen w-screen">
+                <NoteworthySection />
+              </div>
+              <div className="h-screen w-screen">
+                <AchievementSection />
+              </div>
+            </div>
+          </AnimatedLayout>
+        </HorizontalScroll>
       </div>
-    </AnimatedLayout>
+
+      {/* mobile view */}
+      <div className="block lg:hidden">mobile</div>
+    </>
   );
 }
 
