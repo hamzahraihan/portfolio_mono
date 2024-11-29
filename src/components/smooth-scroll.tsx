5f3c2ad5fc50c1981c56d688ff1e7f02860468a3
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-29 16:42:11 +0700

    feat: implement responsive screen width handling in SmoothScroll component

--- src/components/smooth-scroll.tsx
+++ src/components/smooth-scroll.tsx
@@ -2,7 +2,7 @@
 
 import { useSpring, useTransform, useScroll, motion } from 'framer-motion';
 import ResizeObserver from 'resize-observer-polyfill';
-import { ReactNode, useCallback, useLayoutEffect, useRef, useState } from 'react';
+import { ReactNode, useCallback, useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { usePathname } from 'next/navigation';
 type PhysicsProp = {
   damping: number;
@@ -13,8 +13,17 @@ type PhysicsProp = {
 const SmoothScroll = ({ children }: { children: ReactNode }) => {
   const scrollRef = useRef(null);
 
+  const [screenWidth, setScreenWidth] = useState(500);
+  console.log('🚀 ~ SmoothScroll ~ screenWidth:', screenWidth);
+
   const [pageHeight, setPageHeight] = useState(0);
   const pathname = usePathname();
+
+  useEffect(() => {
+    const handleResize = () => setScreenWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   const resizePageHeight = useCallback((entries: ResizeObserverEntry[]) => {
     for (const entry of entries) {
@@ -28,7 +37,7 @@ const SmoothScroll = ({ children }: { children: ReactNode }) => {
       resizeObserber.observe(scrollRef.current);
     }
     return () => resizeObserber.disconnect();
-  }, [scrollRef, resizePageHeight, pathname]);
+  }, [scrollRef, resizePageHeight, pathname, screenWidth]);
 
   const { scrollY } = useScroll();
 
@@ -36,7 +45,7 @@ const SmoothScroll = ({ children }: { children: ReactNode }) => {
   const physics: PhysicsProp = { damping: 15, mass: 0.1, stiffness: 100 };
   const spring = useSpring(transform, physics);
 
-  if (pathname !== '/archives') {
+  if (pathname !== '/archives' || screenWidth <= 1000) {
     return (
       <>
         <motion.div ref={scrollRef} style={{ y: spring }} className="fixed top-0 left-0 w-full overflow-hidden will-change-transform">
