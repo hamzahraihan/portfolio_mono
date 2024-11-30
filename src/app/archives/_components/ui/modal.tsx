303c221d997c9c6203c31a2f7450db596d1adfb0
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-11-30 17:33:37 +0700

    feat: enhance Modal component with improved styling and hover effects for better user interaction

--- src/app/archives/_components/ui/modal.tsx
+++ src/app/archives/_components/ui/modal.tsx
@@ -6,8 +6,13 @@ import { Dialog, DialogContent, DialogTitle, DialogTrigger } from '@/components/
 function Modal({ children, title }: Readonly<{ children: ReactNode; title: string }>) {
   return (
     <Dialog>
-      <DialogTrigger className="dark:bg-neutral-50 bg-neutral-900 dark:text-neutral-800 text-neutral-50 rounded-xl p-2 col-end-3" asChild>
-        <button>{title}</button>
+      <DialogTrigger className="lg:dark:bg-neutral-50 lg:bg-neutral-900 lg:dark:text-neutral-800 lg:text-neutral-50 lg:rounded-xl rounded-md p-2 w-full cursor-pointer transition-all hover:dark:bg-neutral-700 hover:bg-neutral-200" asChild>
+        <div className="group">
+          <div className="transition-all group-hover:translate-x-4">
+            <h1 className="text-lg">{title}</h1>
+            <p className="dark:text-neutral-400 text-sm">Lorem ipsum dolor sit amet consectetur.</p>
+          </div>
+        </div>
       </DialogTrigger>
 
       <DialogTitle hidden></DialogTitle>
