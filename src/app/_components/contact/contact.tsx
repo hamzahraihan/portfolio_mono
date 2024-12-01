7843504b0f84a0a3cf5ceca1b062c0765225ea14
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-12-01 16:38:51 +0700

    fix: adjust ContactSection layout for improved responsiveness and styling consistency

--- src/app/_components/contact/contact.tsx
+++ src/app/_components/contact/contact.tsx
@@ -22,7 +22,7 @@ function ContactSection() {
 
   if (pathname !== '/archives' || screenWidth <= 1000) {
     return (
-      <footer className="flex justify-between p-3 text-sm h-16">
+      <footer className="flex justify-between p-3 text-sm h-16 w-full">
         <div className="flex flex-col gap-1">
           <p>( socials )</p>
           <ul className="flex gap-2 space-y-0">
@@ -52,7 +52,7 @@ function ContactSection() {
           <p>( reach out )</p>
           <div className="flex flex-col">
             <button
-              className="text-neutral-700 dark:text-neutral-500 hover:text-neutral-900 dark:hover:text-neutral-100 duration-100 w-64 text-end lg:text-center"
+              className="text-neutral-700 dark:text-neutral-500 hover:text-neutral-900 dark:hover:text-neutral-100 duration-100 lg:w-64 text-end lg:text-center"
               onClick={handleClipboard}
               onMouseEnter={() => setIsHovering(true)}
               onMouseLeave={() => setIsHovering(false)}
@@ -63,7 +63,7 @@ function ContactSection() {
             <p className={`transition-all ${isClick ? 'opacity-100 visible' : 'invisible opacity-0'} duration-200`}> copied</p>
           </div>
         </div>
-        <div className="hidden  lg:flex items-end gap-1">
+        <div className="hidden lg:flex items-end gap-1">
           <p>hamzahraihan</p>
         </div>
       </footer>
