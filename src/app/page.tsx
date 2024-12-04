d5d34ec5f29705eb763ca3a4affc2ee3fe235105
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-12-04 20:00:19 +0700

    fix: rename HeroPage to HeroSection and adjust main padding for improved layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 'use client';
 
 import { BigText } from './_components/big-text';
-import { HeroPage } from './_components/hero';
+import { HeroSection } from './_components/hero';
 import { AboutPage } from './_components/about';
 import { OtherProjectCTA, MyServicePage } from './_components/my-services';
 import AnimatedLayout from '@/components/animated-presence';
 
 export default function Home() {
   return (
-    <main className="max-w-full h-full">
+    <main className="max-w-full h-full px-2">
       <AnimatedLayout>
-        <HeroPage />
+        <HeroSection />
         <BigText title="software_engineer" />
         <AboutPage />
         <MyServicePage />
