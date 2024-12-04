7968c09818c2ed8c1192607cbacb8133ad45c11e
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-12-04 20:00:28 +0700

    fix: reduce BigText font size for improved readability on smaller screens

--- src/app/_components/big-text/big-text.tsx
+++ src/app/_components/big-text/big-text.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 function BigText({ title = '' }: Readonly<{ title: string }>) {
   return (
     <div className="h-fit overflow-hidden border-b border-neutral-200 dark:border-neutral-800 pb-10">
-      <motion.div className="text-center text-6xl sm:text-7xl md:text-8xl lg:text-[10rem] max-w-full">
+      <motion.div className="text-center text-5xl sm:text-7xl md:text-8xl lg:text-[10rem] max-w-full">
         <span className="animate-flash ">{'>'}</span>
         {title}
       </motion.div>
