1127d2403db53dc3ec802050a9129ef7be741988
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-12-07 13:39:52 +0700

    refactor: simplify Home component by removing unused sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
 'use client';
-
-import { BigText } from './_components/big-text';
 import { HeroSection } from './_components/hero';
-import { AboutPage } from './_components/about';
-import { OtherProjectCTA, MyServicePage } from './_components/my-services';
 import AnimatedLayout from '@/components/animated-presence';
-
 export default function Home() {
   return (
     <main className="max-w-full h-full px-2">
       <AnimatedLayout>
         <HeroSection />
-        <BigText title="software_engineer" />
-        <AboutPage />
-        <MyServicePage />
-        <OtherProjectCTA />
       </AnimatedLayout>
     </main>
   );
