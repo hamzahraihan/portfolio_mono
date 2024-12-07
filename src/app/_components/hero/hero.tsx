de9aab8ce32fd507594a1778babc82f5960bcdad
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-12-07 13:39:57 +0700

    feat: update HeroSection layout and content for improved presentation

--- src/app/_components/hero/hero.tsx
+++ src/app/_components/hero/hero.tsx
@@ -1,21 +1,20 @@
 'use client';
 
 import CircularText from '@/components/circular-text';
-import { Scroll } from '@/components/scroll';
 import { motion } from 'framer-motion';
 
 function HeroSection() {
   return (
-    <div id="hero" className="flex flex-col lg:flex-row max-w-full  h-[50rem] justify-evenly pt-24 mb-10">
-      <motion.div className="flex flex-col gap-5 lg:w-80 h-fit lg:mt-10" initial={{ opacity: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 1.5 }}>
-        <h1 className="text-3xl">Hi, my name is Hamzah Raihan I&apos;m a Full-stack Developer</h1>
-        <p>a highly motivated developer with a passion for mobile and web development. I am currently seeking new opportunities to expand my skills and contribute to the field of technology.</p>
-        <div className="h-10 w-10">
-          <Scroll />
-        </div>
+    <div id="hero" className="flex flex-col max-w-full h-screen justify-between pt-16 px-6 pb-6">
+      <motion.div className="flex flex-col h-fit lg:mt-4" initial={{ opacity: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 1.5 }}>
+        <h1 className="text-2xl lg:text-5xl">Hamzah Raihan</h1>
+        <p>Software Engineer</p>
       </motion.div>
-      <motion.div className="flex justify-center lg:w-fit lg:h-fit overflow-hidden" initial={{ opacity: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 2 }}>
+      <motion.div className="flex justify-center w-full h-fit lg:mt-4" initial={{ opacity: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 1.5 }}>
         <CircularText text="HAMZAHRAIHAN-FULLSTACKDEVELOPER-MOBILE-WEB-" className="motion-safe:animate-spin-slow duration-1000 dark:text-white" />
+      </motion.div>
+      <motion.div className="flex justify-end w-full lg:h-fit overflow-hidden" initial={{ opacity: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 2 }}>
+        <p className="w-80">a highly motivated developer with a passion for mobile and web development. I am currently seeking new opportunities to expand my skills and contribute to the field of technology.</p>
       </motion.div>
     </div>
   );
