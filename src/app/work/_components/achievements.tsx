1430ccbfe7d3e895541519bcc6882e8af13508a7
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-01 15:27:28 +0700

    feat: implement Carousel component for displaying achievements with images in AchievementSection

--- src/app/work/_components/achievements.tsx
+++ src/app/work/_components/achievements.tsx
@@ -6,9 +6,26 @@ import { fetcher } from '@/lib/swr/fetcher';
 import Image from 'next/image';
 import { AchievementProps } from '@/app/api/achievements/route';
 
+function Carousel({ data }: { data: AchievementProps }) {
+  return (
+    <Modal title={data.title} description={data.description}>
+      {data?.image.map((image, index) => (
+        <CarouselItem key={index + 1}>
+          <div className="p-1">
+            <div className="flex aspect-square items-center justify-center p-6">
+              <Image className="object-cover w-full h-56" src={image} alt="achivement image" width={200} height={200} />
+            </div>
+          </div>
+        </CarouselItem>
+      ))}
+    </Modal>
+  );
+}
+
 function AchievementSection() {
   const { data, isLoading } = useSWR('/api/achievements', fetcher);
   console.log(data);
+
   return (
     <div className="h-full w-full py-10 lg:px-10">
       {/* web view */}
@@ -23,99 +40,26 @@ function AchievementSection() {
         <div className="col-span-3 row-end-4 mt-auto">
           <div className="flex flex-col leading-none justify-center">
             <h1 className="uppercase font-semibold text-[6vh] md:text-[7vh] lg:text-[8vh]">Certification</h1>
-            <h1 className="uppercase font-semibold text-[6vh] md:text-[7vh] lg:text-[8vh]">& Achievements</h1>
           </div>
         </div>
 
         <div className="row-end-4 col-end-7 col-span-2">
           <div className="flex justify-evenly items-end w-full h-full">
-            <div className="h-20 w-20 bg-neutral-300 animate-pulse"></div>
-            <div className="h-20 w-20 bg-neutral-300 animate-pulse"></div>
-            <div className="h-20 w-20 bg-neutral-300 animate-pulse"></div>
+            <h1 className="uppercase font-semibold text-[6vh] md:text-[7vh] lg:text-[8vh]">& Achievements</h1>
           </div>
         </div>
 
-        <Modal title="Best project">
-          {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem key={index + 1}>
-              <div className="p-1">
-                <div className="border">
-                  <div className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-4xl font-semibold">{index + 1}</span>
-                  </div>
-                </div>
-              </div>
-            </CarouselItem>
-          ))}
-        </Modal>
-        <Modal title="Best project">
-          {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem key={index + 1}>
-              <div className="p-1">
-                <div className="border">
-                  <div className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-4xl font-semibold">{index + 1}</span>
-                  </div>
-                </div>
-              </div>
-            </CarouselItem>
-          ))}
-        </Modal>
-        <Modal title="Best project">
-          {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem key={index + 1}>
-              <div className="p-1">
-                <div className="border">
-                  <div className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-4xl font-semibold">{index + 1}</span>
-                  </div>
-                </div>
-              </div>
-            </CarouselItem>
-          ))}
-        </Modal>
+        {data?.result.map((item: AchievementProps) => (
+          <Carousel key={item.title} data={item} />
+        ))}
       </div>
       {/* mobile view */}
       <div className="flex flex-col lg:hidden">
         <h1 className="text-4xl border-b pb-4">Certification & Achievements</h1>
         <div className="flex flex-col gap-2 mt-5">
-          <Modal title="Best project">
-            {data?.result.map((item: AchievementProps, i: number) => (
-              <CarouselItem key={i + 1}>
-                <div className="p-1">
-                  <div className="flex aspect-video items-center justify-center p-6">
-                    <Image className="object-cover w-full h-56" src={item.image} alt="achivement image" width={200} height={200} />
-                  </div>
-                </div>
-              </CarouselItem>
-            ))}
-          </Modal>
-          <Modal title="Best project">
-            {Array.from({ length: 5 }).map((_, index) => (
-              <CarouselItem key={index + 1}>
-                <div className="p-1">
-                  <div className="border">
-                    <div className="flex aspect-video h-96 w-48 items-center justify-center p-6">
-                      <span className="text-4xl font-semibold">{index + 1}</span>
-                    </div>
-                  </div>
-                </div>
-              </CarouselItem>
-            ))}
-          </Modal>
-          <Modal title="Best project">
-            {Array.from({ length: 5 }).map((_, index) => (
-              <CarouselItem key={index + 1}>
-                <div className="p-1">
-                  <div className="border">
-                    <div className="flex aspect-square items-center justify-center p-6">
-                      <span className="text-4xl font-semibold">{index + 1}</span>
-                    </div>
-                  </div>
-                </div>
-              </CarouselItem>
-            ))}
-          </Modal>
+          {data?.result.map((item: AchievementProps) => (
+            <Carousel key={item.title} data={item} />
+          ))}
         </div>
       </div>
     </div>
